ad955dbae76e1138d50fe2ad73340215b0f96e49
shatskiym <michael.shatskiy@gmail.com>
2017-03-28 17:10:44 +0300

    Initial Commit

---
+++ WorkWithTurbolinks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkWithTurbolinks
+//
+//  Created by Michael Shatskiy on 28.03.17.
+//  Copyright © 2017 Michael Shatskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
